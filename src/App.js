67e740ab92c63b8d57c5a2a71ed7bcbe34322e15
leeshai-arie <leeshaiarie@gmail.com>
2023-07-30 08:52:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Leeshai Leo",
-      compony: "Lion",
+      monster1: {
+        name: "Linda",
+      },
+      monster2: {
+        name: "Frank",
+      },
+      monster3: {
+        name: "Jacky",
+      },
     }
   }
 
   render() {
     return (
       <div className="App">
-        
+        <h1>{this.state.monster1.name}</h1>
+        <h1>{this.state.monster2.name}</h1>
+        <h1>{this.state.monster3.name}</h1>
       </div>
     );
   }
