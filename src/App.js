c75aa5ed06ae885d5811b3e085c96b6b07d0b58e
leeshai-arie <leeshaiarie@gmail.com>
2023-08-03 15:14:04 +0300

    finshed monstor rolodex

--- src/App.js
+++ src/App.js
@@ -41,7 +41,12 @@ class App extends Component {
 
     return (
       <div className="App">
-        <SearchBox onChangeHandler={onSearchChange}/>
+        <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="search monsters"
+          className="search-box"
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
